99249595c70f0fe243ae7065a92004c837233016
Trang <you@example.>
2024-09-19 16:25:01 +0700

    push base code

---
+++ src/main/java/org/aibles/fashionsalessystem/FashionSalesSystemApplication.java
@@ -0,0 +1,13 @@
+package org.aibles.fashionsalessystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionSalesSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FashionSalesSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/aibles/fashionsalessystem/FashionSalesSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aibles.fashionsalessystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashionSalesSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
